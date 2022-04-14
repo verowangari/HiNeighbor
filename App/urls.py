25fd98ca6360c7f6dfd05da27361146976bd53a8
verowangari <verowangari34@gmail.com>
2022-04-14 10:55:25 +0300

    run login page to test

--- App/urls.py
+++ App/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path,include
 from .import views
+from django.contrib.auth import views as authViews
 
 urlpatterns=[
     path('',views.index,name='index'),
+    path('login/', authViews.LoginView.as_view(template_name='login.html'), name='login'),
 ]
