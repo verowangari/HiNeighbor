d909c5a7e41e85eb24cf47b754ce4fd0bbb070ab
verowangari <verowangari34@gmail.com>
2022-04-14 11:27:30 +0300

    make a responsive login and html file

--- App/views.py
+++ App/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from .forms import SignupForm
 
 # Create your views here.
 def index(request):
@@ -6,3 +8,24 @@ def index(request):
 
 def login(request):
   return render(request, 'login.html')
+
+def Signup(request):
+	if request.method == 'POST':
+		form = SignupForm(request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			email = form.cleaned_data.get('email')
+			password = form.cleaned_data.get('password')
+   
+            
+			User.objects.create_user(username=username, email=email, password=password)
+			return redirect('index')
+        
+	else:
+		form = SignupForm()
+	
+	context = {
+		'form':form,
+	}
+
+	return render(request, 'signup.html', context)
