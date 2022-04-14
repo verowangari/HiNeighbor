ec7d85006936f61d1bd7d3f763b1dd47b57deca3
verowangari <verowangari34@gmail.com>
2022-04-14 18:14:47 +0300

    add a form in profile html

--- App/forms.py
+++ App/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-
+from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
@@ -48,3 +49,16 @@ class SignupForm(forms.ModelForm):
 		if password != confirm_password:
 			self._errors['password'] = self.error_class(['Passwords do not match. Try again'])
 		return self.cleaned_data
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+# Create a ProfileUpdateForm to update image
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
