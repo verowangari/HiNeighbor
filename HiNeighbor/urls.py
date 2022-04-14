767b63a0e400e63e64eaea44de82960bf61bae96
verowangari <verowangari34@gmail.com>
2022-04-14 18:02:01 +0300

    add signals.py

--- HiNeighbor/urls.py
+++ HiNeighbor/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from App.views import profile
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('App.urls')),
+    path('profile/', profile, name='profile'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
