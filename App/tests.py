f96b11419d142a59f9da9f6e8ea00b1b680395d8
verowangari <verowangari34@gmail.com>
2022-04-20 13:02:46 +0300

    add test

--- App/tests.py
+++ App/tests.py
@@ -1,3 +1,65 @@
-from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase
+from .models import Business,NeighbourHood, Profile, Post
+
+# Set up method 
+# Set up method 
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        self.profile=Profile(name='Veronica')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile, Profile))
+
+#Testing Save Method
+
+    
+
+    
+
+# Set up method        
+
+
+class PostTestClass(TestCase):
+    def setUp(self):
+        self.post=Post(post='new post')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.post, Post)) 
+
+
+   
+        
+             
+
+
+
+#  Set up method   
+
+class NeighbourhoodTestClass(TestCase):
+    def setUp(self):
+        self.neighbourhood=NeighbourHood(name='Veronica')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.neighbourhood, NeighbourHood))  
+
+class BusinessTestClass(TestCase):
+    def setUp(self):
+        self.business=Business(email='test@mail.com')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.business, Business))           
+
+
+
+
+
+        
+
+
+
+
+
+        
+
+
